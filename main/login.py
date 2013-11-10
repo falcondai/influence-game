e8d22fbe2749d4fd1b17efea94bf876928fe2807
Falcon Dai <me@falcondai.com>
2013-11-10 15:31:50 -0600

    + hash salted password

--- main/login.py
+++ main/login.py
@@ -1,5 +1,6 @@
 from flask.ext.login import LoginManager, UserMixin
 from bson.objectid import ObjectId
+from Crypto import Random, Hash
 
 from main import app
 from database import mongo
@@ -8,49 +9,57 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 class User(UserMixin):
-	def __init__(self, user_dict=None, **kwargs):
-		if not user_dict:
-			user_dict = kwargs
-		self.id = unicode(user_dict[u'_id'])
-		self.name = user_dict['name']
-		self.password = user_dict['password']
-		self.graph_id = unicode(user_dict['graph_id'])
-		self.obj = user_dict
+    def __init__(self, user_dict=None, **kwargs):
+        if not user_dict:
+            user_dict = kwargs
+        self.id = unicode(user_dict[u'_id'])
+        self.name = user_dict['name']
+        self.salt = user_dict['salt']
+        self.password_hash = user_dict['password_hash']
+        self.graph_id = unicode(user_dict['graph_id'])
+        self.obj = user_dict
 
-	@staticmethod
-	def create_user(name, password):
-		try:
-			gid = mongo.db.graphs.insert({}, j=True)
-			return mongo.db.users.insert({
-				'name': name, 
-				'password': password, 
-				'active': True,
-				'graph_id': gid
-				}, j=True)
-		except:	
-			return None
+    @staticmethod
+    def create_user(name, password, salt_byte_length=32):
+        try:
+            # create an empty graph and obtain its id
+            gid = mongo.db.graphs.insert({}, j=True)
+            salt = Random.get_random_bytes(salt_byte_length).encode('hex')
+            return mongo.db.users.insert({
+                'name': name,
+                'salt': salt,
+                'password_hash': User.hash_password(salt, password), 
+                'active': True,
+                'graph_id': gid
+                }, j=True)
+        except:    
+            return None
 
-	@staticmethod
-	def find_user_by_id(user_id):
-		user_record = mongo.db.users.find_one({u'_id': ObjectId(user_id)})
-		if user_record:
-			return User(user_record)
-		return None
+    @staticmethod
+    def find_user_by_id(user_id):
+        user_record = mongo.db.users.find_one({u'_id': ObjectId(user_id)})
+        if user_record:
+            return User(user_record)
+        return None
 
-	@staticmethod
-	def find_user_by_name(username):
-		user_record = mongo.db.users.find_one({u'name': username})
-		if user_record:
-			return User(user_record)
-		return None
+    @staticmethod
+    def find_user_by_name(username):
+        user_record = mongo.db.users.find_one({u'name': username})
+        if user_record:
+            return User(user_record)
+        return None
 
-	@staticmethod
-	def authenticate_user(username, password):
-		user = User.find_user_by_name(username)
-		if user and password == user.password:
-			return user
-		return None
+    @staticmethod
+    def authenticate_user(username, password):
+        user = User.find_user_by_name(username)
+        if user and User.hash_password(user.salt, password) == user.password_hash:
+            return user
+        return None
+
+    @staticmethod
+    def hash_password(salt, password):
+        return Hash.SHA256.new(salt + password).hexdigest()
 
 @login_manager.user_loader
 def load_user(user_id):
-	return User.find_user_by_id(user_id)
+    return User.find_user_by_id(user_id)
--- main/views.py
+++ main/views.py
@@ -12,117 +12,117 @@ from database import mongo
 login_manager.login_view = 'login'
 
 def handle_redirect(url):
-	return redirect(request.args.get('next') or url)
+    return redirect(request.args.get('next') or url)
 
 def login_form_validate(form):
-	return form.get('username') and form.get('password')
+    return form.get('username') and form.get('password')
 
 def signup_form_validate(form):
-	return form.get('username') and form.get('password')
+    return form.get('username') and form.get('password')
 
 @app.route('/')
 def index():
-	graphs = mongo.db.graphs.find({}, {'_id': 1})
-	return render_template('index.html', graph_ids=map(lambda x: str(x['_id']), graphs))
+    graphs = mongo.db.graphs.find({}, {'_id': 1})
+    return render_template('index.html', graph_ids=map(lambda x: str(x['_id']), graphs))
 
 # user login flow
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-	if request.method == 'POST':
-		if signup_form_validate(request.form):
-			uid = User.create_user(request.form.get('username'), 
-				request.form.get('password'))
-			if uid:
-				flash('signed up successfully. you may log in.' % uid)
-				return handle_redirect(url_for('login'))
-		flash('please check the entered information.')
+    if request.method == 'POST':
+        if signup_form_validate(request.form):
+            uid = User.create_user(request.form.get('username'), 
+                                   request.form.get('password'))
+            if uid:
+                flash('signed up successfully. you may log in.' % uid)
+                return handle_redirect(url_for('login'))
+        flash('please check the entered information.')
 
-	return render_template('signup.html')
+    return render_template('signup.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		if login_form_validate(request.form):
-			user = User.authenticate_user(request.form.get('username'), 
-				request.form.get('password'))
-			if user and login_user(user, 
-				remember=request.form.get('remember')=='on'):
-				flash("Logged in successfully.")
-				return handle_redirect(url_for("index"))	
-		flash("Logged in unsuccessfully.")
+    if request.method == 'POST':
+        if login_form_validate(request.form):
+            user = User.authenticate_user(request.form.get('username'), 
+                                          request.form.get('password'))
+            if user and login_user(user, 
+                remember=request.form.get('remember')=='on'):
+                flash("Logged in successfully.")
+                return handle_redirect(url_for("index"))    
+        flash("Logged in unsuccessfully.")
 
-	return render_template("login.html")
+    return render_template("login.html")
 
 @app.route('/logout')
 @login_required
 def logout():
-	logout_user()
-	flash('Logged out successfully.')
-	return handle_redirect(url_for('index'))
+    logout_user()
+    flash('Logged out successfully.')
+    return handle_redirect(url_for('index'))
 
 # graph views
 @app.route('/view/<graph_id>/<center_node_id>')
 @app.route('/view/<graph_id>/')
 def view(graph_id, center_node_id=''):
-	graph = dumps(_get_graph(graph_id))
-	if not graph:
-		abort(404)
-	if center_node_id == '':
-		center_node_id = _get_center_node(graph_id)
-	# FIXME use a view-only page
-	return render_template('view.html', graph_id=graph_id, center_node_id=center_node_id, graph=graph, all_nodes=dumps([n for n in mongo.db.nodes.find()]))
-	
+    graph = dumps(_get_graph(graph_id))
+    if not graph:
+        abort(404)
+    if center_node_id == '':
+        center_node_id = _get_center_node(graph_id)
+    # FIXME use a view-only page
+    return render_template('view.html', graph_id=graph_id, center_node_id=center_node_id, graph=graph, all_nodes=dumps([n for n in mongo.db.nodes.find()]))
+    
 @app.route('/edit/<graph_id>/<center_node_id>')
 @app.route('/edit/<graph_id>/')
 @login_required
 def edit(graph_id, center_node_id=None):
-	graph = dumps(_get_graph(graph_id))
-	if not graph:
-		abort(404)
-	if not center_node_id:
-		center_node_id = _get_center_node(graph_id)
+    graph = dumps(_get_graph(graph_id))
+    if not graph:
+        abort(404)
+    if not center_node_id:
+        center_node_id = _get_center_node(graph_id)
 
-	if current_user.graph_id != graph_id:
-		flash('You are not the owner of graph %s.' % graph_id)
-		return redirect(url_for('view', graph_id=graph_id, center_node_id=center_node_id))
-	return render_template('modify.html', graph_id=graph_id, center_node_id=center_node_id, graph=graph, all_nodes=dumps([n for n in mongo.db.nodes.find()]))
+    if current_user.graph_id != graph_id:
+        flash('You are not the owner of graph %s.' % graph_id)
+        return redirect(url_for('view', graph_id=graph_id, center_node_id=center_node_id))
+    return render_template('modify.html', graph_id=graph_id, center_node_id=center_node_id, graph=graph, all_nodes=dumps([n for n in mongo.db.nodes.find()]))
 
 # helper functions
 def _get_graph(graph_id):
-	es = mongo.db.edges.find({'graph_id': ObjectId(graph_id)})
-	return [e for e in es]
+    es = mongo.db.edges.find({'graph_id': ObjectId(graph_id)})
+    return [e for e in es]
 
 def _get_node(node_id):
-	return mongo.db.nodes.find_one({'_id': ObjectId(node_id)})
-	
+    return mongo.db.nodes.find_one({'_id': ObjectId(node_id)})
+    
 # TODO not very useful
 # def _inflate_graph(graph):
-# 	nodes = []
-# 	for n_oid in graph['node_oids']:
-# 		nodes.append(mongo.db.nodes.find_one({'_id': n_oid}))
-# 	graph['node_oids'] = nodes
-# 	return graph
+#     nodes = []
+#     for n_oid in graph['node_oids']:
+#         nodes.append(mongo.db.nodes.find_one({'_id': n_oid}))
+#     graph['node_oids'] = nodes
+#     return graph
 
 def _get_center_node(graph_id):
-	g = _get_graph(graph_id)
-	if len(g) > 0:
-		oid = g[0]['source']
-		return unicode(oid)
-	return unicode(mongo.db.nodes.find_one()['_id'])
+    g = _get_graph(graph_id)
+    if len(g) > 0:
+        oid = g[0]['source']
+        return unicode(oid)
+    return unicode(mongo.db.nodes.find_one()['_id'])
 
 # AJAX calls
 # TODO add status message and wrapper to payload
 
 @app.route('/api/graph/<graph_id>.json')
 def get_graph_json(graph_id):
-	# TODO more efficient way to dump json?
-	return Response(dumps(_get_graph(graph_id)), mimetype='application/json')
+    # TODO more efficient way to dump json?
+    return Response(dumps(_get_graph(graph_id)), mimetype='application/json')
 
 @app.route('/api/node/<node_id>.json')
 def get_node_json(node_id):
-	return Response(dumps(_get_node(node_id)), mimetype='application/json')
-	
+    return Response(dumps(_get_node(node_id)), mimetype='application/json')
+    
 @app.route('/api/all_nodes.json')
 def get_all_nodes_json():
-	return Response(dumps([n for n in mongo.db.nodes.find()]), mimetype='application/json')
+    return Response(dumps([n for n in mongo.db.nodes.find()]), mimetype='application/json')
 
